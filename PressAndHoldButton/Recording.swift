0ee8353068bc7c5cbc476c3c77736b73ac3c4dbf
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2019-09-07 16:57:46 -0700

    Refactor recording out of main

---
+++ PressAndHoldButton/Recording.swift
@@ -0,0 +1,109 @@
+//
+//  Recording.swift
+//  PressAndHoldButton
+//
+//  Created by Jason A Faas on 9/7/19.
+//  Copyright © 2019 Jason A Faas. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import Speech
+
+class RecordingForTranslation {
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer:AVAudioPlayer!
+    var primaryLabel: UILabel
+    
+    init(primaryLabel: UILabel) {
+        self.primaryLabel = primaryLabel
+    }
+    
+    func preparePlayer() throws {
+        var error: NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
+        } catch let error1 as NSError {
+            error = error1
+            audioPlayer = nil
+        }
+        if let err = error {
+            primaryLabel.text = "\(String(primaryLabel.text ?? "hello")) Error loading audio to playback."
+            print("AVAudioPlayer error: \(err.localizedDescription)")
+        } else {
+            audioPlayer.delegate = self as? AVAudioPlayerDelegate
+            audioPlayer.prepareToPlay()
+            audioPlayer.volume = 10.0
+            
+            let maxTime:Int = 20
+            if Int(audioPlayer.duration) > maxTime {
+                primaryLabel.text = "\(String(primaryLabel.text ?? "hello"))\nDuration longer than \(maxTime) seconds cannot be transcribed (\(Int(audioPlayer.duration)))..."
+                throw TranscribtionError.durationTooLong(duration: Int(audioPlayer.duration))
+            }
+            
+        }
+    }
+    
+    func playback() {
+        do {
+            try preparePlayer()
+            audioPlayer.play()
+        } catch {
+            
+        }
+    }
+    
+    func startRecording() throws {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+        audioRecorder.delegate = self as? AVAudioRecorderDelegate
+        audioRecorder.record()
+    }
+    
+    func finishRecording() {
+        audioRecorder.stop()
+        audioRecorder = nil
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func getFileURL() -> URL {
+        let path = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        return path as URL
+    }
+    
+    func setupRecordingSession() {
+        
+        do {
+            recordingSession = AVAudioSession.sharedInstance()
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("Recording setup complete")
+                    } else {
+                        // failed to record
+                        print("Failed to Record Level 1")
+                        exit(0)
+                    }}}
+        } catch { // failed to record
+            print("Failed to Record Level 2")
+            exit(0)
+        }
+    }
+}
--- PressAndHoldButton/ViewController.swift
+++ PressAndHoldButton/ViewController.swift
@@ -29,16 +29,19 @@ class ViewController: UIViewController {
     var pinyinToggleText: [Bool: String] = [true: "Turn On Pinyin",
                                             false: "True Off Pinyin", ]
     
+    var translation: RecordingForTranslation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
         if !unitTests() {
             exit(0)
         }
-        // Do any additional setup after loading the view.
         
-        //setup Recorder
-        self.setupView()
+        self.translation = RecordingForTranslation(primaryLabel: self.primaryLabel)
+        
+        self.translation.setupRecordingSession()
         
         self.translationValue = Int.random(in: 0 ..< fullTranslations.count)
 //        self.translationValue = 7
@@ -122,111 +125,22 @@ class ViewController: UIViewController {
         
         primaryLabel.text = "\(String(primaryLabel.text ?? "hello"))\nProcessing..."
         
-        finishRecording(success: true)
+        self.translation.finishRecording()
         
-        do {
-            try preparePlayer()
-            audioPlayer.play()
-            transcribeFile(url: getFileURL() as URL)
-        } catch {
-            
-        }
+        self.translation.playback()
+        self.transcribeFile(url: self.translation.getFileURL() as URL)
+        
         self.buttonTextUpdate.isEnabled = true
-    }
-    
-    func preparePlayer() throws {
-        var error: NSError?
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL() as URL)
-        } catch let error1 as NSError {
-            error = error1
-            audioPlayer = nil
-        }
-        if let err = error {
-            primaryLabel.text = "\(String(primaryLabel.text ?? "hello")) Error loading audio to playback."
-            print("AVAudioPlayer error: \(err.localizedDescription)")
-        } else {
-            audioPlayer.delegate = self as? AVAudioPlayerDelegate
-            audioPlayer.prepareToPlay()
-            audioPlayer.volume = 10.0
-            
-            let maxTime:Int = 20
-            if Int(audioPlayer.duration) > maxTime {
-                primaryLabel.text = "\(String(primaryLabel.text ?? "hello"))\nDuration longer than \(maxTime) seconds cannot be transcribed (\(Int(audioPlayer.duration)))..."
-                throw TranscribtionError.durationTooLong(duration: Int(audioPlayer.duration))
-            }
-            
-        }
     }
 
     
     @IBAction func release(_ sender: Any) {
         primaryLabel.text = "Listening..."
         
-        startRecording()
-    }
-    
-    
-    
-    var recordingSession: AVAudioSession!
-    var audioRecorder: AVAudioRecorder!
-    var audioPlayer:AVAudioPlayer!
-    
-    
-    func setupView() {
-        recordingSession = AVAudioSession.sharedInstance()
         do {
-            try recordingSession.setCategory(.playAndRecord, mode: .default)
-            try recordingSession.setActive(true)
-            recordingSession.requestRecordPermission() { [unowned self] allowed in
-                DispatchQueue.main.async {
-                    if allowed {
-                        self.primaryLabel.text = "recording setup complete"
-                    } else {
-                        // failed to record
-                        self.primaryLabel.text = "Failed to Record Level 1"
-                    }}}
-        } catch { // failed to record
-            primaryLabel.text = "Failed to Record Level 2"
-        }
-    }
-    
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func getFileURL() -> URL {
-        let path = getDocumentsDirectory().appendingPathComponent("recording.m4a")
-        return path as URL
-    }
-    
-    func startRecording() {
-        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
-        
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
-        
-        do {
-            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
-            audioRecorder.delegate = self as? AVAudioRecorderDelegate
-            audioRecorder.record()
-            
-//            recordButton.setTitle("Tap to Stop", for: .normal)
+            try self.translation.startRecording()
         } catch {
-            finishRecording(success: false)
-        }
-    }
-    
-    func finishRecording(success: Bool) {
-        audioRecorder.stop()
-        audioRecorder = nil
-        if !success {
+            self.translation.finishRecording()
             primaryLabel.text = "\(String(primaryLabel.text ?? "hello")) Did not record."
         }
     }
@@ -303,10 +217,7 @@ class ViewController: UIViewController {
                 
             }
         }
-//
-        
     }
-
 
 }
 
