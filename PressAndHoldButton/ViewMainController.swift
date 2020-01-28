861f51d5ff0e230e4a98cddaa5fabfcb1ab55cfa
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2020-01-27 18:52:06 -0800

    Starting to add second view controller

---
+++ PressAndHoldButton/ViewMainController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewMainController.swift
+//  PressAndHoldButton
+//
+//  Created by Jason A Faas on 1/27/20.
+//  Copyright © 2020 Jason A Faas. All rights reserved.
+//
+
+import UIKit
+
+class ViewMainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
