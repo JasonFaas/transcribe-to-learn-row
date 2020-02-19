e92759619af904439d76ae736784769c5160b060
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2020-02-18 18:57:01 -0800

    Halfway through settings controller

---
+++ PressAndHoldButton/ViewSettingsController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewSettingsController.swift
+//  Say Again Mandarin
+//
+//  Created by Jason A Faas on 2/18/20.
+//  Copyright © 2020 Jason A Faas. All rights reserved.
+//
+
+import UIKit
+
+class ViewSettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
